c8f9a9270cb09266d44c70a908ad25a8742c4212
Christopher Mark Gore <cgore@cgore.com>
2021-08-09 20:38:05 -0500

    LimbicCoin contract

---
+++ contracts/LimbicCoin.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract LimbicCoin is ERC20 {
+    constructor() ERC20("LimbicCoin", "LIMBIC") {
+        _mint(msg.sender, 2100000000000000);
+    }
+}
