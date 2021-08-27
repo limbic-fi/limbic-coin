ac86b6a2260bfd3bc4ee0e1f71b0a1512ba6b1bf
Christopher Mark Gore <cgore@cgore.com>
2021-08-26 19:08:40 -0500

    Update LimbicCoin.sol

--- contracts/LimbicCoin.sol
+++ contracts/LimbicCoin.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract LimbicCoin is ERC20 {
-    constructor() ERC20("LimbicCoin", "LIMBIC") {
+    constructor() ERC20("Limbic Coin", "LIMBIC") {
         _mint(msg.sender, 21000000000000000000000000);
     }
 }
